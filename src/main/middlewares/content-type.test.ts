93d2665062015c9537518942ee55de962431f97a
AdrianoDeSiqueira <adrianodizao@hotmail.com>
2020-05-31 15:18:38 -0300

    feat: ensure requests return json as default content type

--- src/main/middlewares/content-type.test.ts
+++ src/main/middlewares/content-type.test.ts
@@ -3,13 +3,21 @@ import app from '../config/app'
 
 describe('Content Type Middleware', () => {
   test('Should return default content type as json', async () => {
-    app.get('/test_cors', (req, res) => {
-      res.send()
+    app.get('/test_content_type', (req, res) => {
+      res.send('')
     })
     await request(app)
-      .get('/test_cors')
-      .expect('access-control-allow-origin', '*')
-      .expect('access-control-allow-headers', '*')
-      .expect('access-control-allow-methods', '*')
+      .get('/test_content_type')
+      .expect('content-type', /json/)
+  })
+
+  test('Should return xml content type when forced', async () => {
+    app.get('/test_content_type_xml', (req, res) => {
+      res.type('xml')
+      res.send('')
+    })
+    await request(app)
+      .get('/test_content_type_xml')
+      .expect('content-type', /xml/)
   })
 })
